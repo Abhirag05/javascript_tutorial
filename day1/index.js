3f67dbbae41182ac5546f76c1935f39ccc04fda3
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-22 12:37:03 +0530

    refactor: organize console logs and comments in index.js

--- day1/index.js
+++ day1/index.js
@@ -1,1 +1,9 @@
 console.log("Hello, World!");
+
+var name = "John Doe";
+console.log("My name is " + name + ".");//inline comment this will not print
+
+/* This is a multi-line comment
+   It can span multiple lines.
+   It will not be executed as code.
+*/
