0014dd34063a48ce3662129c20b97e4d7da1b6a9
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-22 13:43:18 +0530

    data types in js

--- day1/data_types.js
+++ day1/data_types.js
@@ -12,3 +12,37 @@ console.log("Hobbies: " + hobbies);
 console.log("Address: " + JSON.stringify(address));
 console.log("Full Name: " + fullName);
 console.log("Phone Number: " + phoneNumber);
+
+//string indexing and built in methods of string 
+
+string1 = "Hello";
+console.log(string1[0]);//print hello similar as python 
+console.log("Hello"[0]);//same as above
+console.log(string1[string1.length - 1]);//print last character of string
+
+// built in methods of string
+console.log(string1.length); // 5
+console.log(string1.toUpperCase()); // HELLO
+console.log(string1.toLowerCase()); // hello
+
+//numbers in js
+
+var num1 = 1000;
+console.log(num1/(5/5));//print 1000  
+
+//boolean in js
+
+var isTrue = true;
+console.log(isTrue); // true
+console.log(!isTrue); // false
+console.log(30>21); // true
+
+//null and undefined in js
+
+var myVar = null;
+console.log(myVar); // null
+
+var myUndefinedVar;
+console.log(myUndefinedVar); // undefined
+
+console.log(0/0); // NaN (Not a Number)
