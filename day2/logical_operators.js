5b0d17b7f583a6bd1e1a45baa259f1a120f5a0d8
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-24 13:22:58 +0530

    add: implement logical operators example in logical_operators.js

---
+++ day2/logical_operators.js
@@ -0,0 +1,10 @@
+let age=18
+let hasId=false
+
+if (age>=18 && hasId){
+    console.log("You can enter")
+}else if(age>=18 || hasId){
+    console.log("You have to wait")
+}else{
+    console.log("You cant enter")
+}
