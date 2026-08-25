d99b7e443a641759ee9924ad090295357b29cb23
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-25 14:44:52 +0530

    add: implement basic functions and exercise in functions.js

---
+++ day3/functions.js
@@ -0,0 +1,26 @@
+//in js use function keyword similarly def in python
+
+//normal functions
+
+function greet(name) {
+    console.log(`Hello ${name}`)
+}
+
+function main(){
+    return 1
+}
+
+function sum(n1,n2){
+    return n1+n2
+}
+
+console.log(sum(1,5))
+console.log(main())
+greet("abhi")
+greet("zoro")
+
+//excersice
+function convert(usd){
+    return usd*1.5
+}
+console.log(convert(1000))
