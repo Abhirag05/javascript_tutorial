a50f057edb2ae10f530bf791673aa50a263db35c
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-28 13:37:05 +0530

    add: create index.html and dom.js for DOM manipulation examples

---
+++ day5/dom.js
@@ -0,0 +1,13 @@
+//we can change and manipulate the webpage using dom
+
+//1st method(recommended) it is the combination of id,class and all selectors
+console.log(document.querySelector("#heading"))
+
+//2nd method
+console.log(document.getElementById("heading"))
+
+//changing values in the html
+document.querySelector('h1').innerHTML="Welcome to dom"
+
+//changing css
+document.querySelector('h1').style.backgroundColor="red"
