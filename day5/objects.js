33d10b77b877b86e2ab55b55b87dea42ed91eecc
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-28 12:50:05 +0530

    add: create objects.js with user array and console logs

---
+++ day5/objects.js
@@ -0,0 +1,15 @@
+//array of objects
+let user=[
+    {
+    email:"none123@gmail.com",
+    fullname:"none",
+    password:"none123"
+    },
+    {
+    email:"none121@gmail.com",
+    fullname:"none12",
+    password:"none123"
+    },
+]
+console.log(user);
+console.log(user[1].fullname);
