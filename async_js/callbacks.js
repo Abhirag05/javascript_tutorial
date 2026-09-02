213dc5752b14ddba02838e21fdfeac1ab81cced8
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-09-02 12:16:31 +0530

    setinterval fn

--- async_js/callbacks.js
+++ async_js/callbacks.js
@@ -9,3 +9,17 @@ setTimeout(() => {
 //() => {
 //    console.log('');
 //} is the callback function that is passed as an argument to the setTimeout function. It will be executed after 2 seconds.
+
+//another callback example is setInterval function which is used to execute a function repeatedly after a certain interval of time.
+
+const intervalId = setInterval(() => {
+    console.log("This is a setInterval callback function");
+}, 1000);
+
+//to stop the setInterval function we can use clearInterval function and pass the setInterval function as an argument to it.
+
+setTimeout(() => {
+    clearInterval(intervalId);
+    console.log("Stopped the setInterval function afyer 5 seconds");
+}, 5000);
+
